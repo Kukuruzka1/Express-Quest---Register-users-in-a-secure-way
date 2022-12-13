b04c17db3fee9a51ddc5ab236014e1bf6470907a
Kukuruzka1 <irinakukuruza295@gmail.com>
2022-12-13 12:37:08 +0100

    JWT quest done

--- app.js
+++ app.js
@@ -8,26 +8,35 @@ app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
+
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
-
 app.get("/", welcome);
-
+// importing variables from handlers and auth
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 
+//public routes for movies
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+// public routes for users
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
+// public routes for users 
+app.post("/api/login", userHandlers.getUserByEmailWithPasswordAndPassToNext,
+verifyPassword);
+app.post("/api/users", hashPassword, userHandlers.postUser);
+
+app.use(verifyToken); // authentication wall : verifyToken is activated for each route after this line
+// routes to protect for movies
+
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
-const userHandlers = require("./userHandlers");
-const { hashPassword } = require("./auth.js");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id", userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
--- auth.js
+++ auth.js
@@ -1,5 +1,8 @@
 const argon2 = require("argon2");
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
 
+// hashing the password
 const hashingOptions = {
     type: argon2.argon2id,
     memoryCost: 2 ** 16,
@@ -22,6 +25,57 @@ const hashPassword = (req, res, next) => {
    });
   };
 
+  // verifying the password
+  const verifyPassword = (req, res) => {
+    argon2
+    .verify(req.user.hashedPassword, req.body.password)
+    .then((isVerified) => {
+      if (isVerified) {
+        const payload = { sub: req.user.id };
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+
+        delete req.user.hashedPassword;
+        res.send({ token, user: req.user});
+    } else {
+      res.sendStatus(401);
+    }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    })
+  }
+
+  // verifying the token
+  const verifyToken = (req, res, next) => {
+    try{
+      const authHeader = req.get('Authorization'); //get the "Authorization" header from the request
+
+      if (authHeader == null) {
+        throw new Error('Authorization header is missing'); //throw an error if the header is missing
+      }
+
+      const [type, token] = authHeader.split(' '); //split the header string into type and token
+
+      if (type !== "Bearer") {
+        throw new Error("Authorization header does not contain 'Bearer' type"); //throw an error if the type is not "Bearer"
+      }
+
+      req.payload = jwt.verify(token, process.env.JWT_SECRET); //verify the token synchronously: if it's valid, fill req.payload with the decoded payload
+
+      next();
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(401);
+    }
+  };
+
+
   module.exports = {
     hashPassword,
+    verifyPassword,
+    verifyToken,
   };
